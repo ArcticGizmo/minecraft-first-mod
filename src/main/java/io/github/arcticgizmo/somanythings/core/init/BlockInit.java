a821e28f06df8388ee2bd9816c572e35f1b899f0
Jon <jon.howell314@gmail.com>
2022-02-15 18:24:28 +0800

    fixed mineable

--- src/main/java/io/github/arcticgizmo/somanythings/core/init/BlockInit.java
+++ src/main/java/io/github/arcticgizmo/somanythings/core/init/BlockInit.java
@@ -1,9 +1,7 @@
 package io.github.arcticgizmo.somanythings.core.init;
 
 import io.github.arcticgizmo.somanythings.SoManyThings;
-import io.github.arcticgizmo.somanythings.common.blocks.CustomBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -20,20 +18,18 @@ public class BlockInit {
   public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block",
       () -> new Block(BlockBehaviour.Properties
           .of(Material.METAL, MaterialColor.COLOR_BLUE)
-          .strength(15f, 30f)
-          // .harvestTool(ToolType.PICKAXE)
-          // .harvestLevel(1)
+          .strength(2f, 30f)
           .sound(SoundType.METAL)
           .requiresCorrectToolForDrops()));
 
   // public static final RegistryObject<CustomBlock> CUSTOM_BLOCK = BLOCKS.register("custom_block",
-  //     () -> new CustomBlock(BlockBehaviour.Properties
-  //         .of(Material.WOOD)
-  //         // .harvestTool(ToolType.AXE)
-  //         // .harvestLevel(1)
-  //         .sound(SoundType.WOOD)
-  //         .requiresCorrectToolForDrops()));
+  // () -> new CustomBlock(BlockBehaviour.Properties
+  // .of(Material.WOOD)
+  // // .harvestTool(ToolType.AXE)
+  // // .harvestLevel(1)
+  // .sound(SoundType.WOOD)
+  // .requiresCorrectToolForDrops()));
 
   // public static final RegistryObject<Block> EXAMPLE_ORE = BLOCKS.register("example_ore",
-  //     () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
+  // () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
 }
